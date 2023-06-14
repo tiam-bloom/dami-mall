8f297a4e2abe3dfb27bb377f87eeb26b27cfd5b6
Tiam <3036293856@qq.com>
2023-06-14 09:33:04 +0800

    修改product接口访问方式为/product/getInfoById/1

--- dami-mall-springboot/src/main/java/com/zty/xiaomi/server/Controller/ProductController.java
+++ dami-mall-springboot/src/main/java/com/zty/xiaomi/server/Controller/ProductController.java
@@ -3,10 +3,8 @@ package com.zty.xiaomi.server.Controller;
 import com.zty.xiaomi.server.Entity.Product.ProductResult;
 import com.zty.xiaomi.server.Service.Product.ProdServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.IOException;
 
 @CrossOrigin
@@ -16,8 +14,8 @@ public class ProductController {
 
     @Autowired
     private ProdServiceImp prodServiceImp;
-    @RequestMapping("getinfo")
-    public ProductResult getProductInfo(@RequestParam("id") int id) throws IOException {
+    @RequestMapping("/getInfoById/{id}")
+    public ProductResult getProductInfo(@PathVariable("id") int id) throws IOException {
         ProductResult productResult = new ProductResult();
         productResult.setStatus(0);
         productResult.setData(prodServiceImp.getProductInfo(id));
